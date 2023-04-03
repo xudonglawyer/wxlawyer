96bbad6138d71e184ef74d4dcd2785927ad9dc8c
test <test@gmail.com>
2023-04-03 18:46:19 +0800

    text echo service

--- service/lawyer_service.go
+++ service/lawyer_service.go
@@ -4,20 +4,38 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type ResponseMsg struct {
+	ToUserName   string
+	FromUserName string
+	CreateTime   int64
+	MsgType      string
+	Content      string
+}
 
 // CounterHandler 计数器接口
 func LawyerHandler(w http.ResponseWriter, r *http.Request) {
 	res := &JsonResult{}
 
 	if r.Method == http.MethodPost {
-		count, err := modifyCounter(r)
-		if err != nil {
+		decoder := json.NewDecoder(r.Body)
+		body := make(map[string]interface{})
+		if err := decoder.Decode(&body); err != nil {
 			res.Code = -1
 			res.ErrorMsg = err.Error()
 		} else {
-			res.Data = count
+			resp := ResponseMsg{
+				ToUserName:   fmt.Sprint(body["FromUserName"]),
+				FromUserName: fmt.Sprint(body["ToUserName"]),
+				CreateTime:   time.Now().Unix(),
+				MsgType:      fmt.Sprint(body["MsgType"]),
+				Content:      fmt.Sprint(body["Content"]),
+			}
+			res.Data = resp
 		}
+
 	} else {
 		res.Code = -1
 		res.ErrorMsg = fmt.Sprintf("请求方法 %s 不支持", r.Method)
